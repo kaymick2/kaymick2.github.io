7584b78c31b81b162214d0af3c83381744044ab0
kyle mcdowell <kjmcd03@gmail.com>
2026-02-01 13:25:31 -0600

    Fixed broken javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    loadNavbar();
+    loadIncludeHtml();
 });
 
-function loadNavbar() {
-    const navbarContainer = document.querySelector('include-html[src="components/navbar.html"]');
-    if (navbarContainer) {
-        fetch(navbarContainer.getAttribute('src'))
-            .then(response => response.text())
-            .then(data => {
-                navbarContainer.innerHTML = data;
+function loadIncludeHtml() {
+    document.querySelectorAll('include-html[src]').forEach(function(el) {
+        var src = el.getAttribute('src');
+        if (!src) return;
+        fetch(src)
+            .then(function(response) {
+                if (!response.ok) throw new Error(response.status + ' ' + response.statusText);
+                return response.text();
             })
-            .catch(error => console.error('Error loading navbar:', error));
-    }
+            .then(function(html) {
+                el.innerHTML = html;
+            })
+            .catch(function(err) {
+                console.error('Error loading ' + src + ':', err);
+            });
+    });
 }
